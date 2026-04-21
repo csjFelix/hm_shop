42f6cdea6deb3a9be265181fd80f433a4e732d18
Felix <995701034@qq.com>
2026-04-21 20:42:58 +0800

    搭建基础路由和组件

---
+++ lib/routes/index.dart
@@ -0,0 +1,24 @@
+// 管理路由
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+
+// 返回App根级组件
+Widget getRootWidget () {
+  return MaterialApp(
+    initialRoute: "/",
+    routes: getRootRoutes (),
+    
+  );
+
+}
+// 返回该app的路由配置
+Map<String, Widget Function(BuildContext)> getRootRoutes () {
+  return {
+    "/":(context) => MainPage(),
+    "/login":(context) => LoginPage(),
+      
+    
+  };
+
+}
