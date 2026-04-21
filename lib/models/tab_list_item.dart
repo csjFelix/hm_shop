f9f0b698053ab5bff83f8203805fa69ef159f6dc
Felix <995701034@qq.com>
2026-04-21 23:35:04 +0800

    refactor: 用 TabListItem 模型类替换 Map 实现导航栏数据

---
+++ lib/models/tab_list_item.dart
@@ -0,0 +1,12 @@
+class TabListItem {
+  final String icon;
+  final String activeIcon;
+  final String text;
+
+  const TabListItem ({
+    required this.icon,
+    required this.activeIcon,
+    required this.text
+
+  });
+}
--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/models/tab_list_item.dart';
 import 'package:hm_shop/pages/Cart/index.dart';
 import 'package:hm_shop/pages/Category/index.dart';
 import 'package:hm_shop/pages/Home/index.dart';
@@ -14,37 +15,45 @@ class MainPage extends StatefulWidget {
 class _MainPageStateState extends State<MainPage> {
   // 定义数据，根据数据进行渲染4个导航
   // 一般应用程序的导航是不变的
-  final List<Map<String, String>> _tableList = [
-    {
-      "icon": "lib/assets/ic_public_home_normal.png", // 正常显示的图标
-      "active_icon": "lib/assets/ic_public_home_active.png", // 激活显示的图标
-      "text": "首页",
-    },
-    {
-      "icon": "lib/assets/ic_public_pro_normal.png", // 正常显示的图标
-      "active_icon": "lib/assets/ic_public_pro_active.png", // 激活显示的图标
-      "text": "分类",
-    },
-    {
-      "icon": "lib/assets/ic_public_cart_normal.png", // 正常显示的图标
-      "active_icon": "lib/assets/ic_public_cart_active.png", // 激活显示的图标
-      "text": "购物车",
-    },
-    {
-      "icon": "lib/assets/ic_public_my_normal.png", // 正常显示的图标
-      "active_icon": "lib/assets/ic_public_my_active.png", // 激活显示的图标
-      "text": "我的",
-    },
+  // final List<Map<String, String>> _tableList = [
+  //   {
+  //     "icon": "lib/assets/ic_public_home_normal.png", // 正常显示的图标
+  //     "active_icon": "lib/assets/ic_public_home_active.png", // 激活显示的图标
+  //     "text": "首页",
+  //   },
+  //   {
+  //     "icon": "lib/assets/ic_public_pro_normal.png", // 正常显示的图标
+  //     "active_icon": "lib/assets/ic_public_pro_active.png", // 激活显示的图标
+  //     "text": "分类",
+  //   },
+  //   {
+  //     "icon": "lib/assets/ic_public_cart_normal.png", // 正常显示的图标
+  //     "active_icon": "lib/assets/ic_public_cart_active.png", // 激活显示的图标
+  //     "text": "购物车",
+  //   },
+  //   {
+  //     "icon": "lib/assets/ic_public_my_normal.png", // 正常显示的图标
+  //     "active_icon": "lib/assets/ic_public_my_active.png", // 激活显示的图标
+  //     "text": "我的",
+  //   },
+  // ];
+  final List<TabListItem> _tableList = [
+    TabListItem(icon: "lib/assets/ic_public_home_normal.png", activeIcon: "lib/assets/ic_public_home_active.png", text: "首页"),
+    TabListItem(icon: "lib/assets/ic_public_pro_normal.png", activeIcon: "lib/assets/ic_public_pro_active.png", text: "分类"),
+    TabListItem(icon: "lib/assets/ic_public_cart_normal.png", activeIcon: "lib/assets/ic_public_cart_active.png", text: "购物车"),
+    TabListItem(icon: "lib/assets/ic_public_my_normal.png", activeIcon: "lib/assets/ic_public_my_active.png", text: "我的")
 
   ];
+
+
   // 导航栏当前点击的索引
   int _currentIndex = 0;
   List<BottomNavigationBarItem> _getTabBarWidget() {
     return List.generate(_tableList.length, (index) {
       return BottomNavigationBarItem(
-        icon: Image.asset(_tableList[index]["icon"]!, width: 30, height: 30),// 正常显示图标
-        activeIcon: Image.asset(_tableList[index]["active_icon"]!, width: 30, height: 30),// 激活图标
-        label: _tableList[index]["text"]
+        icon: Image.asset(_tableList[index].icon!, width: 30, height: 30),// 正常显示图标
+        activeIcon: Image.asset(_tableList[index].activeIcon!, width: 30, height: 30),// 激活图标
+        label: _tableList[index].text
       );
     });
   }
